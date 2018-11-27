0ffd6747280a1f9a05ff428e1419b499da1fad4c
Sascha Brawer <sascha@brawer.ch>
2018-11-27 10:22:39 +0100

    Add test case for `title` in metadata
    The test case war commented out because the test framework had
    been looking at the wrong data; the underlying bug is now fixed, too.

--- test_split_phrases.py
+++ test_split_phrases.py
@@ -6,14 +6,9 @@
 import codecs
 import os
 import subprocess
+import sys
 import tempfile
 import unittest
-
-
-def read_text_file(directory, filename):
-    path = os.path.join(directory, filename)
-    with codecs.open(path, 'r', encoding='utf-8') as f:
-        return f.read()
 
 
 class TestSplitPhrases(unittest.TestCase):
@@ -25,20 +20,36 @@ class TestSplitPhrases(unittest.TestCase):
                 '--date', '2007-03-09', '--performer', 'Erwin Ardüser',
                 '--organization', 'Lia Rumantscha, Conradin Klais',
                 '--license', 'Creative Commons Zero v1.0 Universal',
-                'testdata/split_phrases'])
+                'testdata/split_phrases'], stderr=sys.stdout.buffer)
             self.assertEqual(read_text_file(workdir, 'fails.txt'),
                              'testdata/split_phrases/bien di.mp3\n')
+            self.assertTrue(exists(workdir, 'jeu-1.flac'))
+            self.assertTrue(exists(workdir, 'savess-1.flac'))
+            self.assertTrue(exists(workdir, 'prender-1.flac'))
             subprocess.check_output([
-                'ffmpeg', '-i', 'test-output/jeu-1.flac', '-f', 'ffmetadata',
-                os.path.join(workdir, 'metadata.txt')])
+                'ffmpeg', '-i', os.path.join(workdir, 'jeu-1.flac'),
+                '-f', 'ffmetadata', os.path.join(workdir, 'metadata.txt')],
+                stderr=sys.stdout.buffer)
             metadata = read_text_file(workdir, 'metadata.txt')
-            # TODO: self.assertIn(r'TITLE=jeu', metadata)
+            self.assertIn(r'TITLE=jeu', metadata)
             self.assertIn(r'PERFORMER=Erwin Ardüser', metadata)
             self.assertIn(r'LANGUAGE=rm-sursilv', metadata)
             self.assertIn(r'DATE=2007-03-09', metadata)
             self.assertIn(r'ORGANIZATION=Lia Rumantscha, Conradin Klais',
                           metadata)
 
+
+# Helper for running unit tests.
+def exists(directory, filename):
+    return os.path.exists(os.path.join(directory, filename))
+
+
+# Helper for running unit tests.
+def read_text_file(directory, filename):
+    path = os.path.join(directory, filename)
+    with codecs.open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+
 if __name__ == '__main__':
     unittest.main()
-
